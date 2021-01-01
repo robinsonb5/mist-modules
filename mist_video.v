76dd1c206c383a50330ff3c9eece6c382ff7be56
Alastair M. Robinson <robinsonb5@gmail.com>
2021-01-01 16:48:48 +0000

    Tidied up mist-video path, ypbpr no longer uses a table, scandoubler clocking is more flexible.

--- mist_video.v
+++ mist_video.v
@@ -1,6 +1,10 @@
 // A video pipeline for MiST. Just insert between the core video output and the VGA pins
 // Provides an optional scandoubler, a rotateable OSD and (optional) RGb->YPbPr conversion
 
+// Adjustments by AMR to reduce block RAM usage and simplify the logic.
+// TODO: take in a blanking signal
+// TODO: delay sync pulses the same amount as the data.
+
 module mist_video
 (
 	// master clock
@@ -15,8 +19,8 @@ module mist_video
 	// scanlines (00-none 01-25% 10-50% 11-75%)
 	input  [1:0] scanlines,
 
-	// non-scandoubled pixel clock divider 0 - clk_sys/4, 1 - clk_sys/2
-	input        ce_divider,
+	// non-scandoubled pixel clock divider 0 - clk_sys/2, 1 - clk_sys/3, 2 - clk_sys/4, etc.
+	input  [2:0] ce_divider,
 
 	// 0 = HVSync 31KHz, 1 = CSync 15KHz
 	input        scandoubler_disable,
@@ -49,9 +53,12 @@ parameter OSD_COLOR    = 3'd4;
 parameter OSD_X_OFFSET = 10'd0;
 parameter OSD_Y_OFFSET = 10'd0;
 parameter SD_HCNT_WIDTH = 9;
+parameter SD_SCNT_WIDTH = 12;
 parameter COLOR_DEPTH = 6; // 1-6
 parameter OSD_AUTO_CE = 1'b1;
 parameter SYNC_AND = 1'b0; // 0 - XOR, 1 - AND
+
+// Scandouble the incoming signal. Scandoubler is bypassed if scandouble_disable is set.
 
 wire [5:0] SD_R_O;
 wire [5:0] SD_G_O;
@@ -59,59 +66,18 @@ wire [5:0] SD_B_O;
 wire       SD_HS_O;
 wire       SD_VS_O;
 
-reg  [5:0] R_full;
-reg  [5:0] G_full;
-reg  [5:0] B_full;
+wire pixel_ena;
 
-always @(*) begin
-	if (COLOR_DEPTH == 6) begin
-		R_full = R;
-		G_full = G;
-		B_full = B;
-	end else if (COLOR_DEPTH == 2) begin
-		R_full = {3{R}};
-		G_full = {3{G}};
-		B_full = {3{B}};
-	end else if (COLOR_DEPTH == 1) begin
-		R_full = {6{R}};
-		G_full = {6{G}};
-		B_full = {6{B}};
-	end else begin
-		R_full = { R, R[COLOR_DEPTH-1 -:(6-COLOR_DEPTH)] };
-		G_full = { G, G[COLOR_DEPTH-1 -:(6-COLOR_DEPTH)] };
-		B_full = { B, B[COLOR_DEPTH-1 -:(6-COLOR_DEPTH)] };
-	end
-end
-
-reg [1:0] i_div;
-reg ce_x1, ce_x2;
-
-always @(posedge clk_sys) begin
-	reg last_hs_in;
-	last_hs_in <= HSync;
-	if(last_hs_in & !HSync) begin
-		i_div <= 2'b00;
-	end else begin
-		i_div <= i_div + 2'd1;
-	end
-end
-
-always @(*) begin
-	if (!ce_divider) begin
-		ce_x1 = (i_div == 2'b01);
-		ce_x2 = i_div[0];
-	end else begin
-		ce_x1 = i_div[0];
-		ce_x2 = 1'b1;
-	end
-end
-
-scandoubler #(SD_HCNT_WIDTH, COLOR_DEPTH) scandoubler
-(
+scandoubler #(
+	.HCNT_WIDTH(SD_HCNT_WIDTH),
+	.HSCNT_WIDTH(SD_SCNT_WIDTH),
+	.COLOR_DEPTH(COLOR_DEPTH))
+scandoubler (
 	.clk_sys    ( clk_sys    ),
 	.scanlines  ( scanlines  ),
-	.ce_x1      ( ce_x1      ),
-	.ce_x2      ( ce_x2      ),
+	.bypass     ( scandoubler_disable ),
+	.pixel_ena  ( pixel_ena  ),
+	.ce_divider ( ce_divider ),
 	.hs_in      ( HSync      ),
 	.vs_in      ( VSync      ),
 	.r_in       ( R          ),
@@ -124,6 +90,10 @@ scandoubler #(SD_HCNT_WIDTH, COLOR_DEPTH) scandoubler
 	.b_out      ( SD_B_O     )
 );
 
+
+// Overlay the on-screen display
+// FIXME - should output delayed Sync pulses
+
 wire [5:0] osd_r_o;
 wire [5:0] osd_g_o;
 wire [5:0] osd_b_o;
@@ -132,30 +102,33 @@ osd #(OSD_X_OFFSET, OSD_Y_OFFSET, OSD_COLOR, OSD_AUTO_CE) osd
 (
 	.clk_sys ( clk_sys ),
 	.rotate  ( rotate  ),
-	.ce      ( scandoubler_disable ? ce_x1 : ce_x2 ),
+	.ce      ( pixel_ena ),
 	.SPI_DI  ( SPI_DI  ),
 	.SPI_SCK ( SPI_SCK ),
 	.SPI_SS3 ( SPI_SS3 ),
-	.R_in    ( scandoubler_disable ? R_full : SD_R_O ),
-	.G_in    ( scandoubler_disable ? G_full : SD_G_O ),
-	.B_in    ( scandoubler_disable ? B_full : SD_B_O ),
-	.HSync   ( scandoubler_disable ? HSync : SD_HS_O ),
-	.VSync   ( scandoubler_disable ? VSync : SD_VS_O ),
+	.R_in    ( SD_R_O ),
+	.G_in    ( SD_G_O ),
+	.B_in    ( SD_B_O ),
+	.HSync   ( SD_HS_O ),
+	.VSync   ( SD_VS_O ),
 	.R_out   ( osd_r_o ),
 	.G_out   ( osd_g_o ),
 	.B_out   ( osd_b_o )
 );
 
+
+// Apply composite video simulation filter, bypassed if blend is low
+
 wire [5:0] cofi_r, cofi_g, cofi_b;
 wire       cofi_hs, cofi_vs;
 
-cofi cofi (
+cofi #(.VIDEO_DEPTH(6)) cofi (
 	.clk     ( clk_sys ),
-	.pix_ce  ( scandoubler_disable ? ce_x1 : ce_x2 ),
+	.pix_ce  ( pixel_ena ),
 	.enable  ( blend   ),
-	.hblank  ( ~(scandoubler_disable ? HSync : SD_HS_O) ),
-	.hs      ( scandoubler_disable ? HSync : SD_HS_O ),
-	.vs      ( scandoubler_disable ? VSync : SD_VS_O ),
+	.hblank  ( ~SD_HS_O ),
+	.hs      ( SD_HS_O ),
+	.vs      ( SD_VS_O ),
 	.red     ( osd_r_o ),
 	.green   ( osd_g_o ),
 	.blue    ( osd_b_o ),
@@ -166,29 +139,37 @@ cofi cofi (
 	.blue_out( cofi_b  )
 );
 
-wire [5:0] y, pb, pr;
 
-rgb2ypbpr rgb2ypbpr
+// Finally convert to YPbPr, bypassed is ypbpr is low
+
+wire [7:0] r_final, g_final, b_final;
+wire hs_final,vs_final;
+
+rgbtoypbpr rgb2ypbpr
 (
-	.red   ( cofi_r  ),
-	.green ( cofi_g  ),
-	.blue  ( cofi_b  ),
-	.y     ( y       ),
-	.pb    ( pb      ),
-	.pr    ( pr      )
+	.clk      ( clk_sys ),
+	.ena      ( ypbpr   ),
+	.red_in   ( {cofi_r,cofi_r[5:4]} ),
+	.green_in ( {cofi_g,cofi_g[5:4]} ),
+	.blue_in  ( {cofi_b,cofi_b[5:4]} ),
+	.hs_in    ( cofi_hs ),
+	.vs_in    ( cofi_vs ),
+	.cs_in    ( SYNC_AND ? (cofi_hs & cofi_vs) : ~(cofi_hs ^ cofi_vs) ),
+	.red_out  ( r_final ),
+	.green_out( g_final ),
+	.blue_out ( b_final ),
+	.hs_out   ( hs_final),
+	.vs_out   ( vs_final),
+	.cs_out   ( cs_final)
 );
 
-assign VGA_R = ypbpr?pr:cofi_r;
-assign VGA_G = ypbpr? y:cofi_g;
-assign VGA_B = ypbpr?pb:cofi_b;
-
-wire   cs = SYNC_AND ? (cofi_hs & cofi_vs) : ~(cofi_hs ^ cofi_vs);
-wire   hs = cofi_hs;
-wire   vs = cofi_vs;
+assign VGA_R = r_final[7:2];
+assign VGA_G = g_final[7:2];
+assign VGA_B = b_final[7:2];
 
 // a minimig vga->scart cable expects a composite sync signal on the VGA_HS output.
 // and VCC on VGA_VS (to switch into rgb mode)
-assign VGA_HS = ((~no_csync & scandoubler_disable) || ypbpr)? cs : hs;
-assign VGA_VS = ((~no_csync & scandoubler_disable) || ypbpr)? 1'b1 : vs;
+assign VGA_HS = ((~no_csync & scandoubler_disable) || ypbpr)? cs_final : hs_final;
+assign VGA_VS = ((~no_csync & scandoubler_disable) || ypbpr)? 1'b1 : vs_final;
 
 endmodule
--- scandoubler.v
+++ scandoubler.v
@@ -18,15 +18,29 @@
 
 // TODO: Delay vsync one line
 
+// AMR - generates and output a pixel clock with a reliable phase relationship with
+// with the scandoubled hsync pulse.  Allows the incoming data to be sampled more
+// sparsely, reducing block RAM usage.  ce_x1/x2 are replaced with a cd_divider
+// which is the largest value the counter will reach before resetting - so 3'111 to
+// divide clk_sys by, 3'011 to divide by 4, 3'101 to divide by six.
+
+// Also now has a bypass mode, in which the incoming data will be scaled to the output
+// width but otherwise unmodified.  Simplifies the rest of the video chain.
+
+
 module scandoubler
 (
 	// system interface
 	input            clk_sys,
 
+	input            bypass,
+
+	// Pixelclock 
+	input      [2:0] ce_divider,
+	output           pixel_ena,
+
 	// scanlines (00-none 01-25% 10-50% 11-75%)
 	input      [1:0] scanlines,
-	input            ce_x1,
-	input            ce_x2,
 
 	// shifter video interface
 	input            hs_in,
@@ -36,15 +50,16 @@ module scandoubler
 	input      [COLOR_DEPTH-1:0] b_in,
 
 	// output interface
-	output reg       hs_out,
-	output reg       vs_out,
-	output reg [5:0] r_out,
-	output reg [5:0] g_out,
-	output reg [5:0] b_out
+	output       hs_out,
+	output       vs_out,
+	output [5:0] r_out,
+	output [5:0] g_out,
+	output [5:0] b_out
 );
 
-parameter HCNT_WIDTH = 9;
-parameter COLOR_DEPTH = 6;
+parameter HCNT_WIDTH = 9; // Resolution of scandoubler buffer
+parameter HSCNT_WIDTH = 12; // Resolution of hsync counters
+parameter COLOR_DEPTH = 6; // Bits per colour to be stored in the buffer
 
 // --------------------- create output signals -----------------
 // latch everything once more to make it glitch free and apply scanline effect
@@ -53,65 +68,84 @@ reg [5:0] r;
 reg [5:0] g;
 reg [5:0] b;
 
+reg [5:0] r_o;
+reg [5:0] g_o;
+reg [5:0] b_o;
+reg hs_o;
+reg vs_o;
+
+wire [COLOR_DEPTH*3-1:0] sd_mux = bypass ? {r_in, g_in, b_in} : sd_out;
+
 always @(*) begin
 	if (COLOR_DEPTH == 6) begin
-		b = sd_out[5:0];
-		g = sd_out[11:6];
-		r = sd_out[17:12];
+		b = sd_mux[5:0];
+		g = sd_mux[11:6];
+		r = sd_mux[17:12];
 	end else if (COLOR_DEPTH == 2) begin
-		b = {3{sd_out[1:0]}};
-		g = {3{sd_out[3:2]}};
-		r = {3{sd_out[5:4]}};
+		b = {3{sd_mux[1:0]}};
+		g = {3{sd_mux[3:2]}};
+		r = {3{sd_mux[5:4]}};
 	end else if (COLOR_DEPTH == 1) begin
-		b = {6{sd_out[0]}};
-		g = {6{sd_out[1]}};
-		r = {6{sd_out[2]}};
+		b = {6{sd_mux[0]}};
+		g = {6{sd_mux[1]}};
+		r = {6{sd_mux[2]}};
 	end else begin
-		b = { sd_out[COLOR_DEPTH-1:0], sd_out[COLOR_DEPTH-1 -:(6-COLOR_DEPTH)] };
-		g = { sd_out[COLOR_DEPTH*2-1:COLOR_DEPTH], sd_out[COLOR_DEPTH*2-1 -:(6-COLOR_DEPTH)] };
-		r = { sd_out[COLOR_DEPTH*3-1:COLOR_DEPTH*2], sd_out[COLOR_DEPTH*3-1 -:(6-COLOR_DEPTH)] };
+		b = { sd_mux[COLOR_DEPTH-1:0], sd_mux[COLOR_DEPTH-1 -:(6-COLOR_DEPTH)] };
+		g = { sd_mux[COLOR_DEPTH*2-1:COLOR_DEPTH], sd_mux[COLOR_DEPTH*2-1 -:(6-COLOR_DEPTH)] };
+		r = { sd_mux[COLOR_DEPTH*3-1:COLOR_DEPTH*2], sd_mux[COLOR_DEPTH*3-1 -:(6-COLOR_DEPTH)] };
 	end
 end
 
 always @(posedge clk_sys) begin
 	if(ce_x2) begin
-		hs_out <= hs_sd;
-		vs_out <= vs_in;
+		hs_o <= hs_sd;
+		vs_o <= vs_in;
 
 		// reset scanlines at every new screen
-		if(vs_out != vs_in) scanline <= 0;
+		if(vs_o != vs_in) scanline <= 0;
 
 		// toggle scanlines at begin of every hsync
-		if(hs_out && !hs_sd) scanline <= !scanline;
+		if(hs_o && !hs_sd) scanline <= !scanline;
 
-		// if no scanlines or not a scanline
-		if(!scanline || !scanlines) begin
-			r_out <= r;
-			g_out <= g;
-			b_out <= b;
+		// if no scanlines, not a scanline, or we're bypassing the scandoubler
+		if(!scanline || !scanlines || bypass) begin
+			r_o <= r;
+			g_o <= g;
+			b_o <= b;
 		end else begin
 			case(scanlines)
 				1: begin // reduce 25% = 1/2 + 1/4
-					r_out <= {1'b0, r[5:1]} + {2'b00, r[5:2] };
-					g_out <= {1'b0, g[5:1]} + {2'b00, g[5:2] };
-					b_out <= {1'b0, b[5:1]} + {2'b00, b[5:2] };
+					r_o <= {1'b0, r[5:1]} + {2'b00, r[5:2] };
+					g_o <= {1'b0, g[5:1]} + {2'b00, g[5:2] };
+					b_o <= {1'b0, b[5:1]} + {2'b00, b[5:2] };
 				end
 
 				2: begin // reduce 50% = 1/2
-					r_out <= {1'b0, r[5:1]};
-					g_out <= {1'b0, g[5:1]};
-					b_out <= {1'b0, b[5:1]};
+					r_o <= {1'b0, r[5:1]};
+					g_o <= {1'b0, g[5:1]};
+					b_o <= {1'b0, b[5:1]};
 				end
 
 				3: begin // reduce 75% = 1/4
-					r_out <= {2'b00, r[5:2]};
-					g_out <= {2'b00, g[5:2]};
-					b_out <= {2'b00, b[5:2]};
+					r_o <= {2'b00, r[5:2]};
+					g_o <= {2'b00, g[5:2]};
+					b_o <= {2'b00, b[5:2]};
 				end
 			endcase
 		end
 	end
 end
+
+// Output multiplexing
+
+assign r_out = bypass ? r_in : r_o;
+assign g_out = bypass ? g_in : g_o;
+assign b_out = bypass ? b_in : b_o;
+assign hs_out = bypass ? hs_in : hs_o;
+assign vs_out = bypass ? vs_in : vs_o;
+
+assign pixel_ena = bypass ? ce_x1 : ce_x2;
+
 
 // scan doubler output register
 reg [COLOR_DEPTH*3-1:0] sd_out;
@@ -127,63 +161,96 @@ reg [COLOR_DEPTH*3-1:0] sd_out;
 reg        line_toggle;
 
 // total hsync time (in 16MHz cycles), hs_total reaches 1024
-reg  [HCNT_WIDTH-1:0] hs_max;
-reg  [HCNT_WIDTH-1:0] hs_rise;
 reg  [HCNT_WIDTH-1:0] hcnt;
+reg  [HSCNT_WIDTH:0] hs_max;
+reg  [HSCNT_WIDTH:0] hs_rise;
+reg  [HSCNT_WIDTH:0] synccnt;
+
+// Input pixel clock, aligned with input sync:
+reg [2:0] i_div;
+wire ce_x1 = (i_div == ce_divider);
 
 always @(posedge clk_sys) begin
 	reg hsD, vsD;
 
+	// Pixel logic on x1 clkena
 	if(ce_x1) begin
-		hsD <= hs_in;
-
-		// falling edge of hsync indicates start of line
-		if(hsD && !hs_in) begin
-			hs_max <= hcnt;
-			hcnt <= 0;
-		end else begin
-			hcnt <= hcnt + 1'd1;
-		end
-
-		// save position of rising edge
-		if(!hsD && hs_in) hs_rise <= hcnt;
-
-		vsD <= vs_in;
-		if(vsD != vs_in) line_toggle <= 0;
-
-		// begin of incoming hsync
-		if(hsD && !hs_in) line_toggle <= !line_toggle;
-
+		hcnt <= hcnt + 1'd1;
 		sd_buffer[{line_toggle, hcnt}] <= {r_in, g_in, b_in};
 	end
+
+	// Generate pixel clock
+	i_div <= i_div + 1'd1;
+
+	if (i_div==ce_divider) i_div <= 3'b000;
+
+	synccnt <= synccnt + 1'd1;
+	hsD <= hs_in;
+	if(hsD && !hs_in) begin
+		hs_max <= {1'b0,synccnt[HSCNT_WIDTH:1]};
+		hcnt <= 0;
+		synccnt <= 0;
+		i_div <= 3'b000;
+	end
+
+	// save position of rising edge
+	if(!hsD && hs_in) hs_rise <= {1'b0,synccnt[HSCNT_WIDTH:1]};
+
+	// begin of incoming hsync
+	if(hsD && !hs_in) line_toggle <= !line_toggle;
+
+	vsD <= vs_in;
+	if(vsD != vs_in) line_toggle <= 0;
+
 end
 
 // ==================================================================
 // ==================== output timing generation ====================
 // ==================================================================
 
+reg  [HSCNT_WIDTH:0] sd_synccnt;
 reg  [HCNT_WIDTH-1:0] sd_hcnt;
 reg        hs_sd;
+
+// Output pixel clock, aligned with output sync:
+reg [2:0] sd_i_div;
+wire ce_x2 = (sd_i_div == ce_divider) | (sd_i_div == {1'b0,ce_divider[2:1]});
 
 // timing generation runs 32 MHz (twice the input signal analysis speed)
 always @(posedge clk_sys) begin
 	reg hsD;
 
+	// Output logic on x2 clkena
 	if(ce_x2) begin
-		hsD <= hs_in;
-
 		// output counter synchronous to input and at twice the rate
 		sd_hcnt <= sd_hcnt + 1'd1;
-		if(hsD && !hs_in)     sd_hcnt <= hs_max;
-		if(sd_hcnt == hs_max) sd_hcnt <= 0;
-
-		// replicate horizontal sync at twice the speed
-		if(sd_hcnt == hs_max)  hs_sd <= 0;
-		if(sd_hcnt == hs_rise) hs_sd <= 1;
-
+		
 		// read data from line sd_buffer
 		sd_out <= sd_buffer[{~line_toggle, sd_hcnt}];
 	end
+	
+	
+	//  Framing logic on sysclk
+	sd_synccnt <= sd_synccnt + 1'd1;
+	hsD <= hs_in;
+	if(hsD && !hs_in) sd_synccnt <= hs_max;
+	
+	if(sd_synccnt == hs_max) begin
+		sd_synccnt <= 0;
+		sd_hcnt <= 0;
+	end
+
+	sd_i_div <= sd_i_div + 1'd1;
+	if (sd_i_div==ce_divider) sd_i_div <= 3'b000;
+
+	// replicate horizontal sync at twice the speed
+	if(sd_synccnt == 0) begin
+		hs_sd <= 0;
+		sd_i_div <= 3'b000;
+	end
+	
+	if(sd_synccnt == hs_rise) hs_sd <= 1;
+
 end
 
 endmodule
